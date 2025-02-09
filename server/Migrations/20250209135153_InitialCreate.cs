ea395abe6b307697a3cc655784728e6e8249141b
Amuniare <trent.brenning@gmail.com>
2025-02-09 08:54:40 -0500

    refresh binary files and update appsettings.json; remove CharacterRepository class

---
+++ server/Migrations/20250209135153_InitialCreate.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VitalityBuilder.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tier = table.Column<int>(type: "int", nullable: false),
+                    MainPoolPoints = table.Column<int>(type: "int", nullable: false),
+                    CombatPoints = table.Column<int>(type: "int", nullable: false),
+                    UtilityPoints = table.Column<int>(type: "int", nullable: false),
+                    SpecialPoints = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CombatAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Focus = table.Column<int>(type: "int", nullable: false),
+                    Power = table.Column<int>(type: "int", nullable: false),
+                    Mobility = table.Column<int>(type: "int", nullable: false),
+                    Endurance = table.Column<int>(type: "int", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CombatAttributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CombatAttributes_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expertise",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expertise", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Expertise_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpecialAttacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AttackType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EffectType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Limits = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Upgrades = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialAttacks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SpecialAttacks_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniquePowers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniquePowers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniquePowers_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UtilityAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Awareness = table.Column<int>(type: "int", nullable: false),
+                    Communication = table.Column<int>(type: "int", nullable: false),
+                    Intelligence = table.Column<int>(type: "int", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UtilityAttributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UtilityAttributes_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombatAttributes_CharacterId",
+                table: "CombatAttributes",
+                column: "CharacterId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expertise_CharacterId",
+                table: "Expertise",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialAttacks_CharacterId",
+                table: "SpecialAttacks",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniquePowers_CharacterId",
+                table: "UniquePowers",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UtilityAttributes_CharacterId",
+                table: "UtilityAttributes",
+                column: "CharacterId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CombatAttributes");
+
+            migrationBuilder.DropTable(
+                name: "Expertise");
+
+            migrationBuilder.DropTable(
+                name: "SpecialAttacks");
+
+            migrationBuilder.DropTable(
+                name: "UniquePowers");
+
+            migrationBuilder.DropTable(
+                name: "UtilityAttributes");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
